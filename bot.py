945b5f20a549d1c9213bb65aa0883ff332c0bc33
Abdumalik <abdumalikabduvohobov341@gmail.com>
2025-06-10 09:19:50 +0300

    lohiya

---
+++ bot.py
@@ -0,0 +1,27 @@
+import asyncio
+from os import getenv
+from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
+from dotenv import load_dotenv
+from handlers import router as handlers_router
+
+load_dotenv()
+
+
+
+TOKEN = getenv("BOT_TOKEN")
+
+dp = Dispatcher()
+dp.include_router(handlers_router)
+
+
+# Run the bot
+async def main() -> None:
+    bot = Bot(token=TOKEN)
+
+    # bot = Bot(token=TOKEN, session = session)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ keyboards.py
@@ -0,0 +1,10 @@
+def stop_btn():
+    return None
+
+
+def start_btn():
+    return None
+
+
+def levels_btn():
+    return None
